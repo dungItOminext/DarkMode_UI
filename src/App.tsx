ae66e29b41eae530a96caef5cc5dc1409282e691
DINH ANH DUNG <dungda@OMINEXT.LOCAL>
2023-01-11 14:24:15 +0700

    DarkMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DarkMode from './DarkMode';
 
 function App() {
   return (
     <div className="App">
+      <DarkMode />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
